6e0ecae56c7443faf20b59854f238b58814f7415
Rankin Kern <rankinkern@Rankins-MacBook-Air.local>
2022-06-25 17:24:15 -0700

    Add unit tests file

---
+++ marsrovertests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿namespace marsrovertests;
+using marsrover;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Grid testGrid = new Grid(5, 5);
+        Assert.AreEqual(5, testGrid.height);
+        Assert.AreEqual(5, testGrid.width);
+    }
+}
