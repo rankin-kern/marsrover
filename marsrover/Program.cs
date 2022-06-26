abe09c2c691c0e088a1d8408ce6208b9f6e07305
Rankin Kern <rankinkern@Rankins-MacBook-Air.local>
2022-06-25 17:17:20 -0700

    adding tests and more src

--- marsrover/Program.cs
+++ marsrover/Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using marsrover;
+
+Console.WriteLine("Enter grid size (e.g 5 5)");
+string? command = Console.ReadLine();
+Grid grid;
+// process grid size command. todo: error handling
+if (command != null)
+{
+    int width = Int32.Parse(command[0].ToString());
+    int height = Int32.Parse(command[2].ToString());
+    
+    grid = new Grid(width, height);
+
+    while ((command = Console.ReadLine()) != null)
+    {
+        // todo: add better validation
+        if (Char.IsNumber(command[0])) {
+            grid.setStartingLocation(command);
+        }
+
+        if (command.StartsWith('L') || command.StartsWith('R') || command.StartsWith('M'))
+        {
+            grid.processInstruction(command);
+        }
+    }
+}
 
