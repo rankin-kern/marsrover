17646eade9a91bfdc1c4a53b9a5879fe27da4140
Rankin Kern <rankinkern@Rankins-MacBook-Air.local>
2022-06-30 09:29:39 -0700

    Add IPlateau

---
+++ marsrover/src/IPlateau.cs
@@ -0,0 +1,17 @@
+﻿using marsrover.commands;
+namespace marsrover
+{
+    public interface IPlateau
+    {
+        public Coordinates Bounds { get; set; }
+        public List<IRover> Rovers { get; }
+
+        // Set the initial position of a rover
+        public void AddRover(StartCommand start);
+
+        public void SetRoverCommands(RoverCommand[] commands);
+
+        public string MoveRovers();
+    }
+}
+
